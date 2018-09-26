dce30909d47ff79035d849cd0b93fb5d2c92998a
Felix Ho <felix.ho@nab.com.au>
2018-09-26 11:56:24 +1000

    Initial Add

---
+++ src/main/java/ho/felix/fhjokesapp/FhJokesAppApplication.java
@@ -0,0 +1,12 @@
+package ho.felix.fhjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FhJokesAppApplication.class, args);
+    }
+}
